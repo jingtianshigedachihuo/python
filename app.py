98dabba669405e6d5894d8f803c4b56e5777ecf3
jingtianshigedachihuo <1074477571@qq.com>
2018-08-21 23:40:33 +0800

    wancheng

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from pymongo  import MongoClient
+client = MongoClient('127.0.0.1',27017)
+mo = client.shiyanlou
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/shiyanlou'
 db = SQLAlchemy(app)
@@ -18,7 +21,36 @@ class File(db.Model):
         self.create_time = create_time
         self.category = category
         self.content = content
-
+    def add_tag(self, tag_name):
+        file_item = mo.files.find_one({'file_id': self.id})
+        if file_item:
+            tags = file_item['tags']
+            if tag_name not in tags:
+                tags.append(tag_name)
+            mo.files.update_one({'file_id': self.id},{'$set': {'tags': tags}})
+        else:
+            tags = [tag_name]
+            mo.files.insert_one({'file_id': self.id,'tags':tags})
+        return tags
+    def remove_tag(self, tag_name):
+        file_item = mo.files.find_one({'file_id': self.d})
+        if file_item:
+            tags = file_item['tags']
+            try:
+                tags.remove(tag_name)
+                new_tags = tags
+            except ValueError:
+                return tags
+            mo.files.update_one({'file_id':self.id},{'$set': {'tags':new_tags}})
+            return new_tags
+        return []
+    @property
+    def tags(self):
+        file_item = mo.files.find_one({'file_id':self.id})
+        if file_item:
+            return file_item['tags']
+        else:
+            return []
 
 class Category(db.Model):
     __tablename__ = 'categorys'
@@ -49,5 +81,6 @@ def file(file_id):
 @app.errorhandler(404)
 def Found_get(e):
     return render_template('404.html'),404
+
 if __name__ == '__main__':
     app.run()
